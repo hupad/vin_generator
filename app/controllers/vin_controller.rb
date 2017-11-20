03385b12ba9777c17beb22e2474f3b37efdd0100
Harish Upadhyayula <hupadhyayula@dealersocket.com>
2017-11-20 15:20:30 -0600

    getting only the first element from the array.

--- app/controllers/vin_controller.rb
+++ app/controllers/vin_controller.rb
@@ -1,7 +1,7 @@
 class VinController < ApplicationController
 
   def index
-    @vin = Vin.limit(1).order("RAND()")
+    @vin = Vin.limit(1).order("RAND()").first
     respond_to do |format|
       format.html
       format.json { render json: @vin.to_json }
